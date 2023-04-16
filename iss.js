21f9a8542bbd882f391007f4c86390d0695d529f
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-04-16 14:49:37 -0700

    Add fetchISSFlyOverTimes function that takes latitude/longitude coordinates and a callback and queries an API for ISS flyover times

--- iss.js
+++ iss.js
@@ -39,5 +39,27 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-// module.exports = { fetchMyIP };
-// module.exports = { fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coordinates, callback) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coordinates.latitude}&lon=${coordinates.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching fly over times. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const parsedBody = JSON.parse(body);
+    const flyOverTimes = parsedBody.response;
+
+    callback(null, flyOverTimes);
+  });
+};
+
+
+module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
+module.exports = { fetchISSFlyOverTimes };
