801f117ba4e2faa62a1e65f06da7458810413e1e
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-04-16 17:59:56 -0700

    Move printPassTimes to iss.js and require it

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const { nextISSTimesForMyLocation } = require('./iss');
-
-const printPassTimes = function(flyOverTimes) {
-  for (const pass of flyOverTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+const { nextISSTimesForMyLocation, printPassTimes } = require('./iss');
 
 nextISSTimesForMyLocation((error, flyOverTimes) => {
   if (error) {
